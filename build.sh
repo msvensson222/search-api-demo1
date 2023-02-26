519471fba48dadf4d86bb09120b58c03d5b4e4ed
Marcus Svensson <marcus.svensson@babyshop.se>
2023-02-26 17:45:31 +0100

    changed build file

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 container_name="demo1-custom-search-api-hybrid"
 docker build -t $container_name .
-docker tag $container_name "eu.gcr.io/strange-song-365307/$container_name"
-docker push "eu.gcr.io/strange-song-365307/$container_name"
+docker tag $container_name "eu.gcr.io/$PROJECT_ID/$container_name"
+docker push "eu.gcr.io/$PROJECT_ID/$container_name"
